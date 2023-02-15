cadd7d516ce7fce92aafae5f7dd05ea74164d42d
maxim.kuzmin <maxim.kuzmin@inbox.ru>
2023-02-15 17:26:36 +0300

    -

---
+++ src/Backend/Common/Core/Integration/IntegrationHelper.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
+
+namespace Makc2023.Backend.Common.Core.Integration;
+
+/// <summary>
+/// Помощник интеграции.
+/// </summary>
+public static class IntegrationHelper
+{
+    #region Public methods
+
+    /// <summary>
+    /// Создать строковое глобально уникальное значение.
+    /// </summary>
+    /// <returns>Глобально уникальное значение.</returns>
+    public static string CreateStringGlobalUniqueValue()
+    {
+        return Guid.NewGuid().ToString().ToUpper();
+    }
+
+    #endregion Public methods
+}
